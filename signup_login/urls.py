5ba1c2d0a4eb6fe8de89b4afacb7400c43721251
karunakar_516 <karunakarbalivada786.com>
2023-10-18 11:58:35 +0530

    added requirements.txt

--- signup_login/urls.py
+++ signup_login/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path,include
-# from .views import RegisterView
 from .views import register,index,user_login,user_logout,addhouse1,search,passreset,otpverify,deleteacc,myhouses
 app_name='signup_login'
 urlpatterns = [
     path('',index,name='home'),
-    path('register',register, name='register'),
-    path('logout',user_logout,name='logout'),
-    path('login',user_login,name='login'),
-    path('addhouse',addhouse1,name='addhouse'),
-    path('search',search,name='search'),
-    path('reset-password',passreset,name='passwordreset'),
-    path('otp-verify',otpverify,name='otpverify'),
-    path('deleteacc',deleteacc,name='deleteacc'),
-    path('myhouses',myhouses,name='myhouses'),
+    path('register/',register, name='register'),
+    path('logout/',user_logout,name='logout'),
+    path('login/',user_login,name='login'),
+    path('addhouse/',addhouse1,name='addhouse'),
+    path('search/',search,name='search'),
+    path('reset-password/',passreset,name='passwordreset'),
+    path('otp-verify/',otpverify,name='otpverify'),
+    path('deleteacc/',deleteacc,name='deleteacc'),
+    path('myhouses/',myhouses,name='myhouses'),
 ]
