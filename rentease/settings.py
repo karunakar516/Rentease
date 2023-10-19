c8eaff417eeef1a0215dac1e7a1276b85bb6fa3b
karunakar_516 <karunakarbalivada786.com>
2023-10-19 10:40:46 +0530

    static

--- rentease/settings.py
+++ rentease/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = 'django-insecure-)75cxssxp4*2cstd+lb!)i0-qrikn3mwtn-it4h_fd1*7o*1!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =[
+    'karunakar516.pythonanywhere.com',
+    'karunakar516.pythonanywhere.com' ,
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -47,7 +51,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,11 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 LOGIN_URL='login/'
